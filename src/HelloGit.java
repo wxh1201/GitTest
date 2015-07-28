11db21b29a7fa3cc3cb795e91310e99f4b3bf487
wxh1201 <Administrator@192.168.1.116>
2015-07-28 10:37:46 +0800

    第三版  编码修改为utf-8

--- src/HelloGit.java
+++ src/HelloGit.java
@@ -3,6 +3,7 @@ public class HelloGit {
      public static void main(String [] args){
     	 System.out.println("hello git");
     	 
-    	 System.out.println("µÚ¶þ°æ");
+    	 System.out.println("ç¬¬ä¸‰ç‰ˆ");
+    	 
      }
 }
